074e319af02638e00c9f36175f8f69c732bedb23
CCChou <83779490+chou999@users.noreply.github.com>
2022-01-06 19:56:48 +0800

    final version

--- guess_a_random_number.py
+++ guess_a_random_number.py
@@ -4,13 +4,16 @@
 # 猜錯，要告訴使用者比答案大或小
 
 import random
-ans = random.randint(1,100)
+start = int(input('請輸入隨機數字範圍起始值： '))
+end = int(input('請輸入隨機數字範圍終止值： '))
+
+ans = random.randint(start,end)
 i = 0
 while True:
-	r = int(input('請輸入一個１～１００間的數字：'))
-	i = i + 1
+	r = int(input('請輸入一個數字：'))
+	i += 1 # i = i + 1
 	if ans == r:
-		print('猜了', i,'次，終於猜對了！！')
+		print('猜了', i, '次，終於猜對了！！')
 		break
 	elif ans > r:
 		print('猜大一點的數字！')
